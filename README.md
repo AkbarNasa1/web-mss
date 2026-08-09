5682fd3d1c7d03871f838fa48eaa97cee59f8525
akbar <akbarnasa5@gmail.com>
2026-08-09 15:48:54 +0700

    web mss

---
+++ README.md
@@ -0,0 +1,35 @@
+# AgenceX - A simple landing page for a digital agency
+
+#### Preview
+
+ - [Demo](https://themewagon.github.io/agenceX/)
+
+#### Download
+ - [Download from ThemeWagon](https://themewagon.com/themes/agenceX/)
+
+## Getting Started
+
+1. Clone Repository
+```
+git clone https://github.com/themewagon/agenceX.git
+```
+2. Install Dependencies
+```
+npm i
+```
+3. Run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
+
+
+## License
+ - Licensed cover under [MIT]
+ - Distributed by <a href="https://themewagon.com" target="_blank">ThemeWagon</a>
---
+++ src/utils/data.ts
@@ -0,0 +1,53 @@
+const services = [
+    {
+        title: "Perdagangan Eceran Hasil Pencetakan dan Penerbitan",
+        description: "menjual eceran barang-barang hasil percetakan dan penerbitan, seperti buku, majalah, koran, kalender, poster, kartu ucapan, dan produk cetakan lainnya kepada konsumen akhir.",
+        // Buku terbuka (book-open)
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
+        </svg>`,
+    },
+    {
+        title: "Perdagangan Eceran Alat Tulis Menulis dan Gambar",
+        description: "menjual eceran alat tulis kantor/sekolah seperti pulpen, pensil, buku tulis, penggaris, alat gambar, dan perlengkapan tulis-menulis lainnya.",
+        // Pensil (pencil)
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+        </svg>`,
+    },
+    {
+        title: "Perdagangan Eceran Pakaian",
+        description: "menjual eceran pakaian jadi (busana) untuk pria, wanita, maupun anak-anak, baik di toko fisik maupun gerai khusus pakaian.",
+        // Tas belanja (shopping bag)
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007z" />
+        </svg>`,
+    },
+    {
+        title: "Periklanan",
+        description: "Mencakup jasa pembuatan dan penempatan iklan, termasuk pembuatan materi iklan (desain, teks, visual), pemasangan iklan di media (cetak, elektronik, luar ruang/billboard), serta jasa konsultasi periklanan dan riset pasar terkait iklan.",
+        // Megafon (megaphone)
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
+        </svg>`,
+    },
+    {
+        title: "Aktivitas Agen Perjalanan Wisata",
+        description: "Usaha jasa agen perjalanan yang menjual tiket transportasi (pesawat, kereta, kapal, bus), paket wisata, reservasi hotel/akomodasi, dan layanan perjalanan lainnya atas nama penyedia jasa (maskapai, hotel, operator tur)",
+        // Pesawat/globe (globe-alt / paper-airplane)
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418" />
+        </svg>`,
+    },
+    {
+        title: "Aktivitas Kebersihan Umum Bangunan",
+        description: "asa kebersihan rutin untuk gedung/bangunan, seperti membersihkan lantai, jendela, ruangan kantor, apartemen, dan fasilitas umum lainnya (cleaning service).",
+        // Kilauan bersih (sparkles)
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M9.813 15.904L9 18.75l-.813-2.846a4.5 4.5 0 00-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 003.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 003.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 00-3.09 3.09zM18.259 8.715L18 9.75l-.259-1.035a3.375 3.375 0 00-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 002.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 002.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 00-2.456 2.456zM16.894 20.567L16.5 21.75l-.394-1.183a2.25 2.25 0 00-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 001.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 001.423 1.423l1.183.394-1.183.394a2.25 2.25 0 00-1.423 1.423z" />
+        </svg>`,
+    },
+    {
+        title: "Real Estat Atas Dasar Balas Jasa (Fee) Atau Kontrak",
+        description: "agen properti (jual-beli/sewa-menyewa properti), manajemen properti, penilaian properti, dan jasa keperantaraan real estat lainnya.",
+        // Gedung (building-office-2)
+        icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"> <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 21h19.5m-18-18v18m10.5-18v18m6-13.5V21M6.75 6.75h.75m-.75 3h.75m-.75 3h.75m3-6h.75m-.75 3h.75m-.75 3h.75M6.75 21v-3.375c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21" />
+        </svg>`,
+    },
+];
+
+export { services };
