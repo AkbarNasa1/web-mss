cce1665c847ed9b170dbe5895a9c093f4b3228d3
akbar <akbarnasa5@gmail.com>
2026-09-14 18:55:11 +0700

    fixed: setting SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
+import sitemap from '@astrojs/sitemap';
 // @ts-check
+
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://mitrasupplyspace.my.id',
+  integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
